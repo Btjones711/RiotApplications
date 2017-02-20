1637812a259476acce27f981838a94858cf649bc
Btjones711 <Btjones711@gmail.com>
2017-02-19 20:25:18 -0600

    -Added some function comments to clarify API pulling functions in RiotAPI

--- RiotAPI.py
+++ RiotAPI.py
@@ -13,18 +13,36 @@ Host = "na.api.pvp.net"
 
 
 def findChampionID(Key, Region):
+    """
+    Function to retrieve Champion ID# based on champion key, usually name(i.e. jax gives 24)
+    only exception I know of at the moment is 'Monkey King' for wukong
+    input: Key, Region strings
+    Output: ID integer
+    """
     url = "https://" + Host + "/api/lol/static-data/" + Region + "/v1.2/champion?api_key=" + API_key
     r = requests.get(url)
     json_obj = r.json()
     return json_obj['data'][Key]['id']
 
 def getChampionData(ChampionID, Region):
+    """
+    Function to retrieve Champion data based on champion ID. Returns stats in form of JSON file. Print it to console
+    to explore more.
+    input: ChampionID integer, Region string
+    Output: Data JSON
+    """
     url = "https://" + Host + "/api/lol/static-data/" + Region + "/v1.2/champion/" + ChampionID + "?api_key=" + API_key
     r = requests.get(url)
     json_obj = r.json()
     return json_obj
 
 def getSummonerID(summonerNames, Region):
+    """
+    Function to retrieve Summoner IDs from a list of summoner names.
+    Returns a list of summoner id's in the same order of names given
+    input: summonerName list[strings], Region string
+    Output: id_list list[integers]
+    """
     names = ""
     for name in summonerNames:
         names = names + "," + name
@@ -38,6 +56,12 @@ def getSummonerID(summonerNames, Region):
     return id_list
 
 def getSummonerNames(summonerIDs, Region):
+    """
+    Function to retrieve summoner names from a list of Summoner IDs.
+    Returns a list of summoner names's in the same order of ID's given
+    input: summonerIDs list[strings], Region string
+    Output: name_list list[integers]
+    """
     ID_list = str(summonerIDs[0])
     for ID in summonerIDs[1:]:
         ID_list += "," + str(ID)
@@ -51,12 +75,20 @@ def getSummonerNames(summonerIDs, Region):
 
 
 def getRecentGames(summonerID, Region):
+    """
+    Function to retrieve Recent games from a Summoner ID.
+    Returns a JSON of recent games. Print to learn more.
+    input: summonerID integer, Region string
+    Output: json_obj JSON
+    """
     summonerID = str(summonerID)
     url = "https://" + Host + "/api/lol/" + Region + "/v1.3/game/by-summoner/" + summonerID + "/recent" + "?api_key=" + API_key
     r = requests.get(url)
     json_obj = r.json()
     return json_obj
 
+
+######Unit tesiting area######
 if __name__ == "__main__":
     test_find = findChampionID("Jax", Region)
     assert(test_find == 24)
